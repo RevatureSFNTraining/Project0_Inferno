011d80389df56a4bcd815c65706ec1505bc0f928
bigcman120 <cliffordmjohnsonjr@gmail.com>
2021-12-15 01:23:56 -0500

    Webpage Built
    The essential functions of the webpage are there, just some styling choices that I'd like to get more in-tune.

--- FaceUpDecks/src/client/modules/my/deckCard/deckCard.js
+++ FaceUpDecks/src/client/modules/my/deckCard/deckCard.js
@@ -1,9 +1,25 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
 
 export default class DeckCard extends LightningElement{
-    deckType = "Blankity";
-    deckDescription = "Something or Other";
-    deckImage = "./resources/kabukijester.jpg";
-    deckPrice = "Expensive";
-    showDescription = true;
+    @api deckType = "Blankity";
+    @api deckDescription = "Something or Other";
+    @api deckImage = "./resources/kabukijester.jpg";
+    @api deckPrice = "Expensive";
+    showDescription = false;
+    @api deckSold = false;
+
+    handleDescriptionVisible(){
+        this.showDescription = true;
+    }
+    
+    handleDescriptionNotVisible(){
+        this.showDescription = false;
+    }
+
+    handleSale(){
+        this.deckImage="./resources/soldout.png"
+        this.deckPrice="Restocking"
+        this.deckDescription="We thank you for your continued patronage and support but, unfortunately, this deck is currently out of stock."
+        this.deckSold=true;
+    }
 }
