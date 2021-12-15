011c7e7e5f45e0cf1299c8daa287948d95e5e001
Anna <aksersch@gmail.com>
2021-12-15 10:00:21 -0500

    More Fine tuning

--- my-app/src/client/modules/my/fpDives/fpDives.js
+++ my-app/src/client/modules/my/fpDives/fpDives.js
@@ -3,14 +3,17 @@ import { LightningElement, track } from 'lwc';
 export default class fpDives extends LightningElement {
 
     @track imgsrc;
+    @track imgsrctwo;
 
     renderedCallback() {
         this.imgsrc = this.template.querySelector('[data-id="A"]');
+        this.imgsrctwo = this.template.querySelector('[data-id="B"]');
     }
 
     page = 1;
-    divetitle = "Drift Diving";
-    diveinfotext = "Follow along with the flow. Take in the awesome scenery. Let our professionally trained instructor's guide you on this next adventure. ";
+    divetitle = "Cave Diving";
+    diveinfotextp1 = "Follow along with the flow. Take in the awesome scenery. Let our professionally trained instructor's guide you on this next adventure. ";
+    diveinfotextp2 = "this is another paragraph of stuff and info!"
 
     handlePrevious() {
         if (this.page > 1) {
@@ -31,14 +34,18 @@ export default class fpDives extends LightningElement {
 
         switch(pages) {
             case 1:
-                this.diveinfotext = "Follow along with the flow. Take in the awesome scenery. Let our professionally trained instructor's guide you on this next adventure. ";
-                this.imgsrc.src = "../../../resources/shipwreck.jpg";
-                this.divetitle = "Drift Diving"
+                this.diveinfotextp1 = "Follow along with the flow. Take in the awesome scenery. Let our professionally trained instructor's guide you on this next adventure. ";
+                this.imgsrc.src = "../../../resources/cavedive1.jpg";
+                this.imgsrctwo.src = "../../../resources/cavedive2.jpg";
+                this.divetitle = "Cave Diving"
                 break;
             case 2:
-                this.diveinfotext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id iaculis urna. Ut in lacus turpis. Nunc lorem neque, finibus eu varius nec, tincidunt ac libero. Aliquam luctus ultrices purus, ac ullamcorper urna lacinia sed. Aliquam tempor finibus orci vel ornare. Mauris pellentesque lorem dolor, quis pellentesque odio malesuada eget. Nullam eget malesuada metus. Pellentesque vel semper urna. Fusce laoreet, leo nec suscipit mattis, lorem odio dictum risus, vitae volutpat quam quam vitae urna. Ut cursus ligula nec dui vulputate porttitor. Praesent ut eros eget erat iaculis lobortis.";
+                this.diveinfotextp1 = `The ocean floor next to our diving location is littered with sunken pirate vessels making for an ideal wreck driving trip. It is reputed as the "pirate's graveyard" and it is said that many years ago the sea and landmarks here possessed an unpredictable phenomena that are not seen anywhere else in the world, many of which made sailing extremely difficult. 
+                The Grand Line used to be the home of some of the world's most notorious pirates, including the past era's "Pirate King".
+                We at Grand Line Diving Inc. offer an exclusive look into these fallen vessels and an unforgettable experience as we gain a glimpse into the long past Pirate Era.`;
                 this.divetitle = "Wreck Driving";
-                this.imgsrc.src = "../../../resources/shipwreck.jpg";
+                this.imgsrc.src = "../../../resources/shipwreck.png";
+                this.imgsrctwo.src = "../../../resources/shipwreck2.png";
                 break;
             default:
                 this.diveinfotext = "there was an error!"
